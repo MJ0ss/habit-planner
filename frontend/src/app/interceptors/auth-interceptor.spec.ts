f0a60da704bae8537c8b1561aacc3427e6795d2d
Michael <michael.joss@stud.hslu.ch>
2026-09-23 20:25:19 +0200

    Add auth interceptor unit tests

--- frontend/src/app/interceptors/auth-interceptor.spec.ts
+++ frontend/src/app/interceptors/auth-interceptor.spec.ts
@@ -1,16 +1,57 @@
 import { TestBed } from '@angular/core/testing';
-import { HttpInterceptorFn } from '@angular/common/http';
+import { provideHttpClient, withInterceptors,} from '@angular/common/http';
+import { provideHttpClientTesting, HttpTestingController,} from '@angular/common/http/testing';
+
+import { HttpClient } from '@angular/common/http';
 import { authInterceptor } from './auth-interceptor';
+import { AuthService } from '../services/auth.service';
 
 describe('authInterceptor', () => {
-  const interceptor: HttpInterceptorFn = (req, next) =>
-    TestBed.runInInjectionContext(() => authInterceptor(req, next));
+  let http: HttpClient;
+  let httpTesting: HttpTestingController;
+  let authService: AuthService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      providers: [
+        provideHttpClient(withInterceptors([authInterceptor])),
+        provideHttpClientTesting(),
+        AuthService,
+      ],
+    });
+
+    http = TestBed.inject(HttpClient);
+    httpTesting = TestBed.inject(HttpTestingController);
+    authService = TestBed.inject(AuthService);
   });
 
-  it('should be created', () => {
-    expect(interceptor).toBeTruthy();
+  afterEach(() => {
+    httpTesting.verify();
+  });
+
+  it('should add the Authorization header when a token exists', () => {
+    authService.token.set('test-token');
+
+    http.get('/api/test').subscribe();
+
+    const req = httpTesting.expectOne('/api/test');
+
+    expect(req.request.headers.get('Authorization')).toBe(
+      'Bearer test-token'
+    );
+
+    req.flush({});
+  });
+
+  it('should not add the Authorization header when no token exists', () => {
+    authService.token.set(null);
+
+    http.get('/api/test').subscribe();
+
+    const req = httpTesting.expectOne('/api/test');
+
+    expect(req.request.headers.has('Authorization')).toBe(false);
+
+    req.flush({});
   });
 });
